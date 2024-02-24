bb5c76dca52a365749becdedb413ceffb6e65ee7
KONETI-JAYANTHVARMA <jayanthvarma09062001@gmail.com>
2024-02-24 17:41:52 +0530

    Captcha Validations Added

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function beforeSubmit() {
-  let outputdate = document.querySelector(".outputdates");
-  let inputdate = document.querySelector(".inputdates");
-  console.log(inputdate.value);
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  console.log(formattedDate);
-  outputdate.value = formattedDate;
+let captchaChecked = false;
+
+function beforeSubmit(event) {
+    if (captchaChecked) {
+        let outputdate = document.querySelector(".outputdates");
+        let inputdate = document.querySelector(".inputdates");
+        console.log(inputdate.value);
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        console.log(formattedDate);
+        outputdate.value = formattedDate;
+    }
+    else {
+        alert("Please check the Captcha Box, for security reasons.");
+        event.preventDefault();
+    }
 }
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
@@ -18,3 +26,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchaSuccess(){
+    captchaChecked = true;
+}
