fdc12eede95c0a5179a01ae54ff03d85ab043ba2
KONETI-JAYANTHVARMA <jayanthvarma09062001@gmail.com>
2024-02-24 16:48:25 +0530

    Added Recaptcha

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function beforeSubmit()
-{
-    let outputdate = document.querySelector(".outputdates");
-    let inputdate = document.querySelector(".inputdates");
-    console.log(inputdate.value);
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    console.log(formattedDate);
-    outputdate.value = formattedDate;
+function beforeSubmit() {
+  let outputdate = document.querySelector(".outputdates");
+  let inputdate = document.querySelector(".inputdates");
+  console.log(inputdate.value);
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  console.log(formattedDate);
+  outputdate.value = formattedDate;
 }
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
